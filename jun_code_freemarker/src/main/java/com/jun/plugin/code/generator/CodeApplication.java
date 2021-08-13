ffa3aa9e13f40300d59c34235cba5e57a3c02cec
俊哥 <wujun728@163.com>
2021-08-14 00:48:35 +0800

    1

---
+++ jun_code_freemarker/src/main/java/com/jun/plugin/code/generator/CodeApplication.java
@@ -0,0 +1,18 @@
+package com.jun.plugin.code.generator;
+
+import org.junit.jupiter.api.Test;
+
+import com.jun.plugin.code.generator.build.TemplateBuilder;
+
+public class CodeApplication {
+
+	public static void main(String[] args) {
+		TemplateBuilder.builder();
+	}
+	
+	@Test
+	public void testname() throws Exception {
+		TemplateBuilder.builder();
+		
+	}
+}
