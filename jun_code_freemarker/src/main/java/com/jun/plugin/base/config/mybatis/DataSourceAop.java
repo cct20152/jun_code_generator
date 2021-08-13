e9dc062c50fd7c559de95cb4525db7dc027d1f24
俊哥 <wujun728@163.com>
2021-08-14 01:44:37 +0800

    1

--- jun_code_freemarker/src/main/java/com/jun/plugin/base/config/mybatis/DataSourceAop.java
+++ jun_code_freemarker/src/main/java/com/jun/plugin/base/config/mybatis/DataSourceAop.java
@@ -18,13 +18,13 @@ public class DataSourceAop {
 
     private static Log logger = LogFactory.getLog(DataSourceAop.class);
 
-    @Before("execution(* com.jun.plugin.mapper..*.find*(..)) || execution(* com.jun.plugin.mapper..*.get*(..))")
+    @Before("execution(* com.jun.plugin.base.mapper..*.find*(..)) || execution(* com.jun.plugin.base.mapper..*.get*(..))")
     public void setReadDataSourceType() {
         DynamicDataSourceContextHolder.useSlaveDataSource();
         logger.info("dataSource切换到：Read");
     }
 
-    @Before("execution(* com.jun.plugin.mapper..*.insert*(..)) || execution(* com.jun.plugin.mapper..*.insert*(..)) || execution(* com.jun.plugin.mapper..*.update*(..))")
+    @Before("execution(* com.jun.plugin.base.mapper..*.insert*(..)) || execution(* com.jun.plugin.base.mapper..*.insert*(..)) || execution(* com.jun.plugin.base.mapper..*.update*(..))")
     public void setWriteDataSourceType() {
         DynamicDataSourceContextHolder.useMasterDataSource();
         logger.info("dataSource切换到：write");
