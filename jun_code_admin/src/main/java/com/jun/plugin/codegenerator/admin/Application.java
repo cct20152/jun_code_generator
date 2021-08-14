b96f25c9aa653440bf50e407a9204866a33ee52b
俊哥 <wujun728@163.com>
2021-08-15 03:10:37 +0800

    1

---
+++ jun_code_admin/src/main/java/com/jun/plugin/codegenerator/admin/Application.java
@@ -0,0 +1,15 @@
+package com.jun.plugin.codegenerator.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+
+}
+
---
+++ jun_code_admin/src/main/java/com/jun/plugin/codegenerator/admin/core/CodeGeneratorTool.java
@@ -0,0 +1,33 @@
+package com.jun.plugin.codegenerator.admin.core;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jun.plugin.codegenerator.admin.core.model.ClassInfo;
+import com.jun.plugin.codegenerator.admin.core.util.GeneratorUtils;
+import com.jun.plugin.codegenerator.admin.core.util.TableParseUtil;
+
+/**
+ * code generate tool
+ */
+public class CodeGeneratorTool {
+
+	/**
+	 * process Table Into ClassInfo
+	 *
+	 * @param tableSql
+	 * @return
+	 */
+	public static ClassInfo processTableIntoClassInfo(String tableSql) throws IOException {
+		return TableParseUtil.processTableIntoClassInfo(tableSql);
+	}
+	
+	public static List<ClassInfo> processMetadataClassInfo(String tablename) throws IOException {
+		List<ClassInfo> list = new ArrayList<ClassInfo>();
+		GeneratorUtils.builderClassInfo(list);
+		return list;
+	}
+
+}
