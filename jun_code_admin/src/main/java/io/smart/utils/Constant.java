9ab0efed5289961bc627329debd81be03983329a
wujun728 <wujun728@163.com>
2022-12-28 23:53:24 +0800

    1

---
+++ jun_code_admin/src/main/java/io/smart/utils/Constant.java
@@ -0,0 +1,13 @@
+package io.smart.utils;
+
+/**
+ * 常量
+ * 
+ * @author hermit
+ * @email chenqiang_cool@126.com
+ * @date 2016年11月15日 下午1:23:52
+ */
+public class Constant {
+
+
+}
---
+++ jun_code_admin/src/main/java/io/smart/utils/PageResult.java
@@ -0,0 +1,31 @@
+package io.smart.utils;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+
+/**
+ * 
+ * @author xiaosf
+ * @date 2017年8月16日
+ */
+@Data
+public class PageResult<T> implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 分页数据
+	 */
+	private List<T> data=new ArrayList<T>();
+	
+	/**
+	 * 分页信息
+	 */
+	private Page page=new Page();
+}
