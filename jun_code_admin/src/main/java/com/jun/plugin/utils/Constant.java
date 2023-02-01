6565b3491a5d96ec84e8c3f6b47a179828ea0f92
wujun728 <wujun728@hotmail.com>
2023-02-01 10:29:04 +0800

    1

---
+++ jun_code_admin/src/main/java/com/jun/plugin/utils/Constant.java
@@ -0,0 +1,13 @@
+package com.jun.plugin.utils;
+
+/**
+ * 常量
+ * 
+ * @author hermit
+ * @email chenqiang_cool@126.com
+ * @date 2016年11月15日 下午1:23:52
+ */
+public class Constant {
+
+
+}
---
+++ jun_code_admin/src/main/java/com/jun/plugin/utils/Page.java
@@ -0,0 +1,32 @@
+package com.jun.plugin.utils;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+/**
+ * 分页参数实体类
+ *
+ * @author hermit
+ * @date 2017 -06-28 19:51:23
+ */
+@Data
+public class Page implements Serializable {
+    /**
+	 * 
+	 */
+    private static final long serialVersionUID = 1L;
+    
+    private int page = 1;// 当前页索引
+    
+    private int pageSize = 20;// 每页显示的数据条数
+    
+    private int total = 0;// 总条数
+    
+    private int totalPage = 1;// 总页数
+    
+    // private String sort;// 排序字段
+    //
+    // private String order;// 排序方式 ASC DESC
+    
+}
